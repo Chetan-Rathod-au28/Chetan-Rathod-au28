92cc89aeb200ce9687041551b79bc68763e9d524
chetan rathod <chetanrathod600@gmail.com>
2021-12-02 18:52:55 +0530

    add

---
+++ week1/day1.py
@@ -0,0 +1,25 @@
+1] Brief discription of commands are as follows:
+Answer-
+   1)git pull:i)Git Pull is used to fetch and merge changes from remote repository to the local repository.
+              ii)Git Pull is combination of two commands,git fetch followed by git merge.
+   2)git push:to push your changes from local branch to cloud repository.
+   3)git merge :this command is to merge all branch to the master branch.
+   4)git log :it is command which tell you about history of commits.
+   5)git status:it is use to display state of repository.it shows us that file is in staging or unstaged area.it
+   6)git commit: it is used to move files from staging area to commit.
+   7)git init:it shows initialized empty git repository/directory/folder. 
+   8)git clone:it will be used first time to clone the repository from cloud to your local. 
+   9)git branch:it is use to create,list,rename and delete branches.
+   10)git checkout:git checkout lets you navigate between the branches created by git branch.
+
+2]simple calculator +,_,*,/ support in python.
+Answer-
+    print("enter first value")
+      a = int(input())
+    print("enter second value")
+      b = int(input())
+   print("addition of both values",a+b)
+   print("substraction of both values",a-b)
+   print("multiplication of both values",a*b)
+   print("division of both values",a/b) 
+
---
+++ week1/day2.py
@@ -0,0 +1,25 @@
+1]program that takes floating point number as input and prints its integer part only.
+answer-
+    print("enter values in decimal")
+     a =float(input())
+     print("integer value is",int(a))
+2] simple program to explain(using components) the working of type() function.
+answer-
+    type(43)
+<class 'int'>
+    type(-3.3)
+<class 'float">
+    type("hello")
+<class 'str'>
+type(print)
+<class 'builtin_function_or_method'>
+3]simple calculator with +,_,*,/ support in python use input() function to take inputs from user.
+answer-
+    print("enter first value")
+      a = int(input())
+    print("enter second value")
+      b = int(input())
+   print("addition of both values",a+b)
+   print("substraction of both values",a-b)
+   print("multiplication of both values",a*b)
+   print("division of both values",a/b) 
